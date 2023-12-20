86ecc3ac791049786300d0b72db7e3e8c76e9ff2
Jan Hubert Ochea <Janhubertochea14.com>
2023-12-21 07:33:34 +0800

    commit sa js sa log in

---
+++ log in.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const logAcctpage = document.querySelector(".box");
+    const createAcctpage = document.querySelector(".box1");
+
+    document.getElementById('logAcct').addEventListener('click',function (){
+        logAcctpage.style.display = 'block';
+        createAcctpage.style.display = 'none';
+    });
+
+    document.getElementById('createAcct').addEventListener('click',function (){
+        logAcctpage.style.display = 'none';
+        createAcctpage.style.display = 'block';
+    });
+});
+
+
+
+
+document.getElementById('SignIn').addEventListener('click', function () {
+    const payr = parseInt(document.getElementsByClassName('inputBXx').value);
+
+    if (pay) {
+        alert('PLEASE FILL OUT THE FORM  :(')
+    } else {
+        alert('Account Created!')
+    }
+});
